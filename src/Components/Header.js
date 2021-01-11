7f14bc3d63a826b33048f42fc1c6a5150052d913
MatthewRenda <MatthewJRenda@gmail.com>
2021-01-11 15:18:47 -0800

    visual update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,10 +11,10 @@ class Header extends Component {
 
                 <div className='headerTextContainer'>
                     <h1 style={{fontSize:'180%',color:'white'}}>
-                        Full stack developer with a passion for creating, and a drive to learn.
+                    Hi, my name is Matthew Renda.
                     </h1>
                     <h4 style={{fontSize:'130%',color:'white'}}>
-                        Hi, my name is Matthew Renda. I look forward to working with you.
+                    Full stack developer.
                     </h4>
                 </div>
                 <div className='header-logo-container'>
@@ -22,11 +22,11 @@ class Header extends Component {
                 </div>
             
             </div>
-            <div class="custom-shape-divider-top-1610247056">
+            <div class="custom-shape-divider-top-1610406512">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
-               </svg>
-           </div>
+                    <path d="M649.97 0L550.03 0 599.91 54.12 649.97 0z" class="shape-fill"></path>
+                </svg>
+            </div>
         
             {/* <ContactPage/> */}
                
