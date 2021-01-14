3cfde7d1a39990d0c083993ac3fdbe6491d2585c
MatthewRenda <MatthewJRenda@gmail.com>
2021-01-14 00:10:15 -0800

    visual update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -14,8 +14,11 @@ class Footer extends Component {
                      </svg>
                 </div>
                     <div className='footer-bgColor center blue'>
-                        <img className='footer-center footer-img' src={require('../imgs/portfolio_logo_yellow.png')}
-                            alt="Matthew Renda logo"/>
+                        <a href="https://matthewrenda.com">
+                            <img className='footer-center footer-img' src={require('../imgs/portfolio_logo_yellow.png')}
+                                alt="Matthew Renda logo"/>
+                        </a>
+                        
                         <div className='footer-txtCenter'> Copyright &copy; 2020, MatthewJRenda</div>
 
 
