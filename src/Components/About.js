d4401396cf9a6611687b0564357901d862eebe15
MatthewRenda <MatthewJRenda@gmail.com>
2021-08-06 13:34:06 -0700

    adds alt text to images

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,20 +10,20 @@ class About extends Component {
 
                 <div className='aboutContainer'>
                     <div className='item'>
-                        <img  className='about-img' src={require('../imgs/school.jpg')}></img>
+                        <img  className='about-img' alt="Education" src={require('../imgs/school.jpg')}></img>
                         <h4><b>Education</b></h4>
                         <p className='left'> I attended California State University, Sacramento.
                             Graduating with a Bachelors in Computer Science.</p> 
                     </div>
                     <div className='item'>
-                        <img className='about-img' src={require('../imgs/growth.jpg')}></img>
+                        <img className='about-img' alt="Passion" src={require('../imgs/growth.jpg')}></img>
                         <h4><b>Passion</b></h4>
                         <p className='left'>Seeing something through from start to finish has always given me a sense of
                         fullfillment. Developing is a wonderful way to create, and add value
                         to many peoples lives.</p>
                     </div>
                     <div className='item'>
-                        <img className='about-img' src={require('../imgs/mind.jpg')}></img>
+                        <img className='about-img' alt="Drive" src={require('../imgs/mind.jpg')}></img>
                         <h4><b>Drive</b></h4>
                         <p className='left'>I have grown quite a thirst for knowledge, and knowing that my work can always
                         improve makes me strive to be better every day.</p>
