cd800f4771e3ae7e3300a377fb2e87d537a350c2
MatthewRenda <MatthewJRenda@gmail.com>
2022-01-18 10:11:42 -0800

    explicit img size

--- src/Components/Utils/Intro.js
+++ src/Components/Utils/Intro.js
@@ -7,7 +7,7 @@ const Intro = ({header,description,img})=>{
                 <h1 class="display-5 fw-bold lh-1 mb-3">{header}</h1>
                 <p class="lead">{description}</p>
             </div>
-                {img? <img src={img} class="d-block mx-lg-auto img-fluid" alt="img" width="500" height="auto" loading="lazy"/>: ''}
+                {img? <img src={img} class="d-block mx-lg-auto img-fluid" alt="img" width="500" height="500" loading="lazy"/>: ''}
         </div>
     )
 }
