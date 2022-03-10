5e4ef6dd409304000277fa33d9fb0eb6e40a0621
MatthewRenda <MatthewJRenda@gmail.com>
2022-03-09 22:09:58 -0800

    updates pricing page

--- src/Components/Pages/Pricing/Pricing.js
+++ src/Components/Pages/Pricing/Pricing.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ContactPage from '../../Utils/ContactPage';
+import React , {useEffect, useState} from 'react';
 import Lottie from 'react-lottie';
 import ExampleAnimation from '../../../lotties-animations/ExampleAnimation.json';
 import Components from '../../../lotties-animations/components.json';
+import Border from '../../../lotties-animations/border.json';
 import ButtonGame from '../../Utils/ButtonGame';
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
-const Pricing = () => {
+const Pricing = () => {    
+    const [selectedContent,setSelectedContent] = useState('hiding')
+    const [selectedAnimations,setSelectedAnimations] = useState('hiding')
+    const [selectedLogos,setSelectedLogos] = useState('hiding')
+    const [selectedComponents,setSelectedComponents] = useState('hiding')
     return (
         <div className=''>
             <div style={{ position: 'absolute', backgroundColor: '#f9eff1', width: '100%', height: '650px', top: '0px', left: '0px', zIndex: 0 }}></div>
@@ -34,67 +38,118 @@ const Pricing = () => {
                 <button id='start' className='btn' onClick={()=>{document.getElementById('start').scrollIntoView({behavior:'smooth'})}} style={{position:'absolute',bottom:'0px'}}><KeyboardDoubleArrowDownIcon style={{fontSize:'200%', fill:'#508096'}}/></button>
             </div>
             <div  className='mt-5 mb-5'>
-                <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', alignItems: 'center',padding:'50px 20px' }}>
-                    <div style={{fontSize:'170%', textAlign:'center'}}>
-                        <h2 style={{fontSize:'150%', color: '#508096' }}>Static Content</h2>
-                        <p>(Text and images per page)</p>
-                        <span style={{ color: '#508096', position: 'relative', top: '-15px', fontWeight: 'lighter' }}>$</span>
-                            <b style={{ color: '#508096', fontSize: '150%' }}>100</b>
+                <div style={{display: 'flex',flexWrap:'wrap',justifyContent:'space-evenly', padding:'50px 20px' }}>
+                    <div style={{width:'300px',height:'300px', display: 'flex', alignItems:'center', flexDirection:'column',textAlign:'center', cursor:'pointer'}}>
+                        <div  style={{position:'relative',height:'350px', width:'320px'}}>
+                            <div className={selectedContent}>
+                                <Lottie
+                                    style={{position:'absolute', top:'-10px',zIndex:-1}}
+                                    options={{
+                                        loop: true,
+                                        autoplay: true,
+                                        animationData: Border
+                                    }}
+                                /> 
+                            </div>
+                            <div onClick={()=>setSelectedContent(selectedContent == 'visible'? 'hiding' : 'visible')}>
+                                <h2 style={{fontSize:'150%', color: '#508096' }}>Static Content</h2>
+                                <p style={{margin:'0px'}}>(Text and images per page)</p>
+                                <img alt='static Example' width={'250px'} height={'auto'} src='/imgs/Img_text_Example.png' />
+                                <div style={{marginTop:'10px'}}>
+                                    <span style={{ color: '#508096', position: 'relative', top: '-15px', fontWeight: 'lighter' }}>$</span>
+                                    <b style={{ color: '#508096', fontSize: '150%' }}>100</b>
+                                </div>
+                            </div>
+                        </div>  
+                      
                     </div>
-                    <img alt='static Example' width={'300px'} height={'auto'} src='/imgs/Img_text_Example.png' />
-                </div>
-                <div style={{backgroundColor:'#035946', display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', alignItems: 'center', padding:'50px 20px' }}>
-                    <div style={{fontSize:'170%', textAlign:'center'}}>
-                        <h2 style={{fontSize:'150%', color: 'white' }}>Animations</h2>
-                        <p>
-                            <span style={{ color: 'white', position: 'relative', top: '-15px', fontWeight: 'lighter' }}>$</span>
-                            <b style={{ color: 'white', fontSize: '250%' }}>200</b>
-                        </p>
+                    <div style={{width:'300px',height:'300px', display: 'flex', alignItems:'center', flexDirection:'column',textAlign:'center', cursor:'pointer'}}>
+                        <div  style={{position:'relative',height:'350px', width:'320px'}}>
+                            <div className={selectedAnimations}>
+                                <Lottie
+                                    style={{position:'absolute', top:'-10px',zIndex:-1}}
+                                    options={{
+                                        loop: true,
+                                        autoplay: true,
+                                        animationData: Border
+                                    }}
+                                /> 
+                            </div>
+                            <div onClick={()=>setSelectedAnimations(selectedAnimations == 'visible'? 'hiding' : 'visible')}>
+                                <h2 style={{fontSize:'150%', color: '#508096' }}>Animations</h2>
+                                <div style={{ textAlign: 'center' }}>
+                                    <Lottie
+                                        options={{
+                                            loop: true,
+                                            autoplay: true,
+                                            animationData: ExampleAnimation
+                                        }}
+                                        height={'200px'}
+                                        width={'200px'}
+                                    />
+                                </div>
+                            </div>
+                            <div style={{marginTop:'10px'}}>
+                                <span style={{ color: '#508096', position: 'relative', top: '-15px', fontWeight: 'lighter' }}>$</span>
+                                <b style={{ color: '#508096', fontSize: '150%' }}>200</b>
+                            </div>
+                        </div>
                     </div>
-                    <div style={{ textAlign: 'center' }}>
-                        <Lottie
-                            options={{
-                                loop: true,
-                                autoplay: true,
-                                animationData: ExampleAnimation
-                            }}
-                            height={'200px'}
-                            width={'200px'}
-                        />
+                    <div style={{width:'300px',height:'300px', display: 'flex', alignItems:'center', flexDirection:'column',textAlign:'center', cursor:'pointer'}}>
+                        <div  style={{position:'relative',height:'350px', width:'320px'}}>
+                            <div className={selectedLogos}>
+                                <Lottie
+                                    style={{position:'absolute', top:'-10px',zIndex:-1}}
+                                    options={{
+                                        loop: true,
+                                        autoplay: true,
+                                        animationData: Border,
+                                    }}
+                                /> 
+                            </div>
+                            <div onClick={()=>setSelectedLogos(selectedLogos == 'visible'? 'hiding' : 'visible')}>
+
+                                <h2 style={{ fontSize: '150%',color: '#508096' }}>Logos</h2>
+                                <div style={{ textAlign: 'center' }}>
+                                    <img alt='logo Example' width={'250px'} height={'auto'} src='/imgs/Example_logo.png' />
+                                </div>
+                                <div style={{marginTop:'30px'}}>
+                                    <span style={{ color: '#508096', position: 'relative', top: '-15px', fontWeight: 'lighter' }}>$</span>
+                                    <b style={{ color: '#508096', fontSize: '150%' }}>150</b>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', alignItems: 'center',padding:'50px 20px' }}>
-                    <div style={{fontSize:'170%', textAlign:'center'}}>
-                        <h2 style={{ fontSize: '150%',color: '#508096' }}>Logos</h2>
-                        <p>
-                            <span style={{ color: '#508096', position: 'relative', top: '-15px', fontWeight: 'lighter' }}>$</span>
-                            <b style={{ color: '#508096', fontSize: '250%' }}>150</b>
-                        </p>
-                    </div>
-                    <div style={{ textAlign: 'center' }}>
-                        <img alt='logo Example' style={{ borderRadius: '100%' }} width={'200px'} height={'auto'} src='/imgs/Example_logo.png' />
-                    </div>
-                </div>
-                <div style={{backgroundColor:'#f6f6f6', display: 'flex', justifyContent:'space-around', flexDirection: 'column', alignItems: 'center',padding:'50px 20px' }}>
-                    <div style={{fontSize:'170%', textAlign:'center'}}>
-                        <h2 style={{fontSize: '150%', color: '#508096' }}>Logical Components</h2>
-                        <p>(Components built to "do" something)</p>
-                        <b style={{ color: '#508096', fontSize: '100%' }}>As low as</b>
-                        <span style={{ color: '#508096', position: 'relative', top: '-15px', fontWeight: 'lighter' }}>$</span>
-                        <b style={{ color: '#508096', fontSize: '150%' }}>100</b>
-                    </div>
-                    <div style={{ width: '300px', height: '200px', position: 'relative', alignItems: 'center' }}>
-                        <ButtonGame />
+                    <div style={{width:'300px',height:'300px', display: 'flex', alignItems:'center', flexDirection:'column',textAlign:'center', cursor:'pointer'}}>
+                        <div  style={{position:'relative',height:'350px', width:'320px'}}>
+                            <div className={selectedComponents}>
+                                <Lottie
+                                    style={{position:'absolute', top:'-10px',zIndex:-1}}
+                                    options={{
+                                        loop: true,
+                                        autoplay: true,
+                                        animationData: Border
+                                    }}
+                                /> 
+                            </div>
+                            <div onClick={()=>setSelectedComponents(selectedComponents == 'visible'? 'hiding' : 'visible')}>
+                                <h2 style={{fontSize: '150%', color: '#508096' }}>Logical Components</h2>
+                                <p>(Components built to "do" something)</p>
+                                <div style={{width: '300px',height:'40px', position: 'relative' }}>
+                                    <ButtonGame />
+                                </div>
+                                <div style={{marginTop:'120px'}}>
+                                    <b style={{ color: '#508096', fontSize: '100%' }}>As low as</b>
+                                    <span style={{ color: '#508096', position: 'relative', top: '-15px', fontWeight: 'lighter' }}>$</span>
+                                    <b style={{ color: '#508096', fontSize: '150%' }}>100</b>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            {/* <div className='mt-5 mb-5' style={{ display: 'flex', justifyContent: 'center', }}>
-                GET A QUOTE
-            </div> */}
-            <div className='mt-5 mb-5' style={{ display: 'flex', justifyContent: 'center', }}>
-                <ContactPage title={'Message us to get started!'} />
-            </div>
+           
         </div>
     )
 }
--- src/Components/Utils/ButtonGame.js
+++ src/Components/Utils/ButtonGame.js
@@ -1,20 +1,23 @@
 import React, { useEffect } from 'react';
 const ButtonGame =()=>{
-        useEffect(()=>{
-            const chest = document.querySelector('#btnmover');
-            chest.onmouseover = move;
-            chest.onclick = move;
-        },[])
+        let chest; 
 
-       function move(){
-        const chest = document.querySelector('#btnmover');
-        chest.style.top = Math.floor(Math.random()*200)+'px';
-        chest.style.left =  Math.floor(Math.random()*200)+'px';
-       }
-      
+        function move(){
+          chest.style.top = Math.floor(Math.random()*20)+'px';
+          chest.style.left =  Math.floor(Math.random()*100)+'px';
+        }
+        
+        const moving = ()=>{
+          chest = document.querySelector('#btnmover');
+          setInterval(()=>{
+            move();
+          },1000)
+          
+        }
+        window.onload = moving;
       return(
         <div id='btnmover' class="btnStyle" style={{backgroundColor:'#508096', color:'white', borderRadius:'10px', padding:'20px'}}>
-            Click me!
+            I can move!
         </div>
       )
 
